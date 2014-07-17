c3ee1de9e2197fe2812b818f25d2fc59fab9a13e
Erik Unger <erik@erikunger.com>
2014-07-17 20:56:36 +0200

    HandlePOST recognizes ct application/json, xml, pain/text

--- server.go
+++ server.go
@@ -1,8 +1,125 @@
+/*
+## go-rest A small and evil REST framework for Go
+
+### Reflection, Go structs, and JSON marshalling FTW!
+
+* go get github.com/ungerik/go-rest
+* import "github.com/ungerik/go-rest"
+* Documentation: http://go.pkgdoc.org/github.com/ungerik/go-rest
+* License: Public Domain
+
+Download, build and run example:
+
+	go get github.com/ungerik/go-rest
+	go install github.com/ungerik/go-rest/example && example
+
+Small?
+
+Yes, the framework consists of only three functions:
+HandleGET, HandlePOST, RunServer.
+
+Evil?
+
+Well, this package can be considered bad design because
+HandleGET and HandlePOST use dynamic typing to hide 36 combinations
+of handler function types to make the interface _easy_ to use.
+36 static functions would have been more lines of code but
+dramatic _simpler_ in their individual implementations.
+So simple in fact, that there wouldn't be a point in
+abstracting them away in an extra framework.
+See this great talk about easy vs. simple:
+http://www.infoq.com/presentations/Simple-Made-Easy
+Rob Pike may also dislike this approach:
+https://groups.google.com/d/msg/golang-nuts/z4T_n4MHbXM/jT9PoYc6I1IJ
+So yes, this package can be called evil because it is an
+anti-pattern to all that is good and right about Go.
+
+Why use it then? By maximizing dynamic code
+it is easy to use and reduces code.
+Yes, that introduces some internal complexity,
+but this complexity is still very low in absolute terms
+and thus easy to control and debug.
+The complexity of the dynamic code also does not spill over
+into the package users' code, because the arguments and
+results of the handler functions must be static typed
+and can't be interface{}.
+
+Now let's have some fun:
+
+HandleGET uses a handler function that returns a struct or string
+to create the GET response. Structs will be marshalled as JSON,
+strings will be used as body with auto-detected content type.
+
+Format of GET handler:
+
+	func([url.Values]) ([struct|*struct|string][, error]) {}
+
+Example:
+
+	type MyStruct struct {
+		A in
+		B string
+	}
+
+	rest.HandleGET("/data.json", func() *MyStruct {
+		return &MyStruct{A: 1, B: "Hello World"}
+	})
+
+	rest.HandleGET("/index.html", func() string {
+		return "<!doctype html><p>Hello World"
+	})
+
+The GET handler function can optionally accept an url.Values argument
+and return an error as second result value that will be displayed as
+500 internal server error if not nil.
+
+Example:
+
+	rest.HandleGET("/data.json", func(params url.Values) (string, error) {
+		v := params.Get("value")
+		if v == "" {
+			return nil, errors.New("Expecting GET parameter 'value'")
+		}
+		return "value = " + v, nil
+	})
+
+HandlePOST maps POST form data or a JSON document to a struct that is passed
+to the handler function. An error result from handler will be displayed
+as 500 internal server error message. An optional first string result
+will be displayed as a 200 response body with auto-detected content type.
+
+Suported content types for POST requests are:
+* application/x-www-form-urlencoded
+* multipart/form-data
+* text/plain
+* application/json
+* application/xml
+
+Format of POST handler:
+
+	func([*struct|url.Values]) ([struct|*struct|string],[error]) {}
+
+Example:
+
+	rest.HandlePOST("/change-data", func(data *MyStruct) (err error) {
+		// save data
+		return err
+	})
+
+Both HandleGET and HandlePOST also accept one optional object argument.
+In that case handler is interpreted as a method of the type of object
+and called accordingly.
+
+Example:
+
+	rest.HandleGET("/method-call", (*myType).MethodName, myTypeObject)
+*/
 package rest
 
 import (
 	"bytes"
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -125,8 +242,8 @@ func HandlePOST(path string, handler interface{}, object ...interface{}) {
 	switch len(in) {
 	case 1:
 		a := in[0]
-		if a != urlValuesType && (a.Kind() != reflect.Ptr || a.Elem().Kind() != reflect.Struct) {
-			panic(fmt.Errorf("HandlePOST(): first handler argument must be a struct pointer or url.Values, got %s", a))
+		if a != urlValuesType && (a.Kind() != reflect.Ptr || a.Elem().Kind() != reflect.Struct) && a.Kind() != reflect.String {
+			panic(fmt.Errorf("HandlePOST(): first handler argument must be a struct pointer, string, or url.Values. Got %s", a))
 		}
 		httpHandler.getArgs = func(request *http.Request) []reflect.Value {
 			ct := request.Header.Get("Content-Type")
@@ -172,23 +289,50 @@ func HandlePOST(path string, handler interface{}, object ...interface{}) {
 				return []reflect.Value{s}
 
 			case "text/plain":
-				if a == urlValuesType {
-					panic(fmt.Errorf("HandlePOST(): first handler argument must be a struct pointer when request Content-Type is text/plain, got %s", a))
+				if a.Kind() != reflect.String {
+					panic(fmt.Errorf("HandlePOST(): first handler argument must be a string when request Content-Type is text/plain, got %s", a))
 				}
-				s := reflect.New(a.Elem())
 				defer request.Body.Close()
-				j, err := ioutil.ReadAll(request.Body)
+				body, err := ioutil.ReadAll(request.Body)
 				if err != nil {
 					panic(err)
 				}
-				err = json.Unmarshal(j, s.Interface())
+				return []reflect.Value{reflect.ValueOf(string(body))}
+
+			case "application/xml":
+				if a.Kind() != reflect.Ptr || a.Elem().Kind() != reflect.Struct {
+					panic(fmt.Errorf("HandlePOST(): first handler argument must be a struct pointer when request Content-Type is application/xml, got %s", a))
+				}
+				s := reflect.New(a.Elem())
+				defer request.Body.Close()
+				body, err := ioutil.ReadAll(request.Body)
+				if err != nil {
+					panic(err)
+				}
+				err = xml.Unmarshal(body, s.Interface())
+				if err != nil {
+					panic(err)
+				}
+				return []reflect.Value{s}
+
+			case "application/json":
+				if a.Kind() != reflect.Ptr || a.Elem().Kind() != reflect.Struct {
+					panic(fmt.Errorf("HandlePOST(): first handler argument must be a struct pointer when request Content-Type is application/json, got %s", a))
+				}
+				s := reflect.New(a.Elem())
+				defer request.Body.Close()
+				body, err := ioutil.ReadAll(request.Body)
+				if err != nil {
+					panic(err)
+				}
+				err = json.Unmarshal(body, s.Interface())
 				if err != nil {
 					panic(err)
 				}
 				return []reflect.Value{s}
 
 			case "multipart/form-data":
-				if a == urlValuesType {
+				if a.Kind() != reflect.Ptr || a.Elem().Kind() != reflect.Struct {
 					panic(fmt.Errorf("HandlePOST(): first handler argument must be a struct pointer when request Content-Type is multipart/form-data, got %s", a))
 				}
 				file, _, err := request.FormFile("JSON")
@@ -197,11 +341,11 @@ func HandlePOST(path string, handler interface{}, object ...interface{}) {
 				}
 				s := reflect.New(a.Elem())
 				defer file.Close()
-				j, err := ioutil.ReadAll(file)
+				body, err := ioutil.ReadAll(request.Body)
 				if err != nil {
 					panic(err)
 				}
-				err = json.Unmarshal(j, s.Interface())
+				err = json.Unmarshal(body, s.Interface())
 				if err != nil {
 					panic(err)
 				}
